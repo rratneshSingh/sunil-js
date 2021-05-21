7a3280537c3965f3d25d6a79fc813d37e45cf2b1
ratnesh singh <dev.ratnesh.singh@gmail.com>
2021-05-21 23:28:25 +0530

    promise explabntion

--- promise/main.js
+++ promise/main.js
@@ -3,6 +3,7 @@ var users = [];
 var promise = new Promise( function( resolve , reject ) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
+        console.log( this.readyState );
         if (this.readyState == 4 ) {
             if ( this.status === 200 ) {
                 resolve( this.responseText );
@@ -20,3 +21,19 @@ promise.then( function( result ) {
     users = result; 
     console.log( users );
 });
+
+
+
+// Promise( callback ) {
+//     var successCallback;
+//     this.then = function ( thenCallback ) {
+//         successCallback = thenCallback;
+//         return this;
+//     }
+//     var resolve = function( result ) {
+//         successCallback( result );
+//     }
+//      var reject = function () {
+//     }
+//     callback( resolve, reject  );
+// }
